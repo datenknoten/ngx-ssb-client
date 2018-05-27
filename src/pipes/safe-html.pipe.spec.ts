a9e5595d5ef4e50fd2ff541d2c7c9a14d62580b7
Tim Schumacher <tim@datenknoten.me>
2018-05-27 10:30:39 +0200

    ci: add testing with jest

--- src/pipes/safe-html.pipe.spec.ts
+++ src/pipes/safe-html.pipe.spec.ts
@@ -1,8 +1,50 @@
-import { SafeHtmlPipe } from './safe-html.pipe';
+/**
+ * @license MIT
+ */
+
+// tslint:disable: no-submodule-imports
+
+import {
+    inject,
+    TestBed,
+} from '@angular/core/testing';
+import {
+    BrowserModule,
+    DomSanitizer,
+} from '@angular/platform-browser';
+import {
+    BrowserTestingModule,
+} from '@angular/platform-browser/testing';
+import {
+    expect,
+} from 'chai';
+
+import {
+    SafeHtmlPipe,
+} from './safe-html.pipe';
 
 describe('SafeHtmlPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SafeHtmlPipe();
-    expect(pipe).toBeTruthy();
-  });
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                BrowserModule,
+                BrowserTestingModule,
+            ],
+        });
+    });
+
+
+    it('create an instance', inject([DomSanitizer], (domSanitizer: DomSanitizer) => {
+        const pipe = new SafeHtmlPipe(domSanitizer);
+        expect(pipe).to.be.instanceof(SafeHtmlPipe);
+    }));
+
+    it('should create a safehtml construct', inject([DomSanitizer], (domSanitizer: DomSanitizer) => {
+        const pipe = new SafeHtmlPipe(domSanitizer);
+        const html = '<div><a href="ssb://ssb/@foobar">Link</a></div>';
+        const output = pipe.transform(html);
+        expect(output).to.be.an('object');
+        expect(output.constructor.name).to.be.equal('SafeHtmlImpl');
+    }));
 });
--- src/test.ts
+++ src/test.ts
@@ -1,20 +1,18 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+/**
+ * @license MIT
+ */
 
-import 'zone.js/dist/zone-testing';
+require('jest-preset-angular');
+// tslint:disable-next-line: no-submodule-imports
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from '@angular/platform-browser-dynamic/testing';
-
-declare const require: any;
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting()
-);
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
+beforeEach(() => {
+    getTestBed().configureCompiler({ preserveWhitespaces: false } as any);
+});
+Object.defineProperty(document.body.style, 'transform', {
+    value: () => {
+        return {
+            enumerable: true,
+            configurable: true,
+        };
+    },
+});
