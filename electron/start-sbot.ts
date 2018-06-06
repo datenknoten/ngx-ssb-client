197da59a97e237b257663437b39e4e7bbc74ebc9
Tim <tim@datenknoten.me>
2018-06-06 19:19:28 +0200

    Update dependencies (#28)
    update dependencies and enable ebt

--- electron/start-sbot.ts
+++ electron/start-sbot.ts
@@ -29,6 +29,7 @@ const createSbot = window.require('scuttlebot')
     .use(window.require('ssb-search'))
     .use(window.require('ssb-names'))
     .use(window.require('ssb-ws'))
+    .use(window.require('ssb-ebt'))
     .use(window.require(path.resolve(__dirname, '../dist-electron/blob-mimetype')));
 
 // tslint:disable-next-line:no-default-export
